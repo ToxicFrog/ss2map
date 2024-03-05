d4c0ecfb9baad4fc94ed5e8fd572369c70e9b87a
Rebecca Kelly <bk@ancilla.ca>
2024-03-04 19:38:02 -0500

    Proper command line parsing for main, make object dumping optional

--- main.lua
+++ main.lua
@@ -3,23 +3,54 @@ require 'util'
 local vstruct = require 'vstruct'
 local tagfile = require 'db.tagfile'
 
-local function hexdump(s)
-  return s:gsub('.', function(c) return string.format('%02X', c:byte()) end)
+flags.register('help', 'h', '?') {
+  help = 'display this text';
+}
+
+flags.register('gamesys') {
+  help = 'path to gamesys, e.g. shock.gam';
+  type = flags.string;
+}
+
+flags.register('objects') {
+  help = 'list all objects in the loaded map';
+}
+
+flags.register('props') {
+  help = 'list all object properties, not just name/position';
+}
+
+flags.register('inherited') {
+  help = 'when listing object properties, include inherited properties';
+}
+
+local function listobjs(mis, props, inherited)
+  for id,brush in pairs(mis.chunks.BRLIST.by_type[-3]) do
+    local pos = brush.position
+    local name = mis:getProp(brush.primal, 'SymName', true)
+    printf('[%08x] %s @ (%f,%f,%f)\n',
+        brush.primal, name, pos.x, pos.y, pos.z)
+    if props then
+      for k,v,inherited in mis:propPairs(brush.primal, inherited) do
+        printf('  %s %-16s %s\n', inherited and '+' or ' ', k..':', v)
+      end
+    end
+  end
 end
 
 local function main(...)
-  local mis = tagfile(...)
-
-  -- chunk names appear to be globally unique within the file, which is convenient
-  for _,entry in ipairs(mis.toc) do
-    printf('%08X %8d %s\n', entry.offset, entry.size, entry.tag)
+  local args = flags.parse {...}
+  if args.help or #args < 1 then
+    print('Usage: mismap [flags] [--gamesys=shock.gam] map.mis')
+    print(flags.help())
+    os.exit(1)
   end
 
-  for id,props in pairs(mis.props) do
-    print('OBJPROP', id)
-    for k,v in pairs(props) do
-      print('', k, hexdump(v))
-    end
+  local gam; if args.gamesys then gam = tagfile(args.gamesys) end
+  local mis = tagfile(args[1], gam)
+
+  if args.objects then
+    listobjs(mis, args.props, args.inherited)
   end
 
   return mis
