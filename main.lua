78e605e8262152688fc67bb1b3ba5c1be0a24193
Rebecca Kelly <bk@ancilla.ca>
2024-03-06 17:03:51 -0500

    Split map viewing and map object listing into separate tools

--- main.lua
+++ main.lua
@@ -18,32 +18,6 @@ flags.register('proplist') {
   type = flags.string;
 }
 
-flags.register('objects') {
-  help = 'list all objects in the loaded map';
-}
-
-flags.register('props') {
-  help = 'list all object properties, not just name/position';
-}
-
-flags.register('inherited') {
-  help = 'when listing object properties, include inherited properties';
-}
-
-local function listobjs(mis, props, inherited)
-  for id,brush in pairs(mis.chunks.BRLIST.by_type[-3]) do
-    local pos = brush.position
-    local name = mis:getProp(brush.primal, 'SymName', true)
-    printf('[%08x] %s @ (%f,%f,%f)\n',
-        brush.primal, name, pos.x, pos.y, pos.z)
-    if props then
-      for k,v,inherited in mis:propPairs(brush.primal, inherited) do
-        printf('  %s %-16s %s\n', inherited and '+' or ' ', k..':', v)
-      end
-    end
-  end
-end
-
 local function main(...)
   local args = flags.parse {...}
   if args.help or #args < 1 then
@@ -60,10 +34,6 @@ local function main(...)
 
   local gam; if args.gamesys then gam = tagfile(args.gamesys) end
   local mis = tagfile(args[1], gam)
-
-  if args.objects then
-    listobjs(mis, args.props, args.inherited)
-  end
 
   return mis
 end
