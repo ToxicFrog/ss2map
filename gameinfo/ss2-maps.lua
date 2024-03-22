74e729afd382c36d80f731088cbb464b76344ba8
Rebecca Kelly <bk@ancilla.ca>
2024-03-22 15:06:11 -0400

    Update SS2 level names based on SAVENAME.STR

--- gameinfo/ss2-maps.lua
+++ gameinfo/ss2-maps.lua
@@ -1,32 +1,31 @@
-defmap 'UNN Recruitment Facility' { short = '🌐'; files = 'earth.mis'; }
+defmap 'UNN Ramsey Centre' { short = '🌐'; files = 'earth.mis'; }
 defmap 'UNN Dispatch Station' { short = '🛰'; files = 'station.mis'; }
 
-defmap 'Engineering 1' { short = '🔧1'; files = 'eng1.mis'; }
-defmap 'Engineering 2' { short = '🔧2'; files = 'eng2.mis'; }
+defmap 'Engineering A' { short = '🔧1'; files = 'eng1.mis'; }
+defmap 'Engineering B' { short = '🔧2'; files = 'eng2.mis'; }
 
-defmap 'Med/Sci 1' { short = '🧪1'; files = 'medsci1.mis'; }
-defmap 'Med/Sci 2' { short = '🧪2'; files = 'medsci2.mis'; }
+defmap 'Med/Sci - Science' { short = '🧪1'; files = 'medsci1.mis'; }
+defmap 'Med/Sci - Medical' { short = '🧪2'; files = 'medsci2.mis'; }
 
-defmap 'Hydroponics 1' { short = '🌿1'; files = 'hydro1.mis'; }
-defmap 'Hydroponics 2' { short = '🌿2'; files = 'hydro2.mis'; }
-defmap 'Hydroponics 2' { short = '🌿3'; files = 'hydro3.mis'; }
+defmap 'Hydroponics A' { short = '🌿1'; files = 'hydro1.mis'; }
+defmap 'Hydroponics B/C' { short = '🌿2'; files = 'hydro2.mis'; }
+defmap 'Hydroponics D' { short = '🌿3'; files = 'hydro3.mis'; }
 
-defmap 'Operations 1' { short = '🖧1'; files = 'ops1.mis'; }
-defmap 'Operations 2' { short = '🖧2'; files = 'ops2.mis'; }
-defmap 'Operations 3' { short = '🖧3'; files = 'ops3.mis'; }
-defmap 'Operations 4' { short = '🖧4'; files = 'ops4.mis'; }
+defmap 'Operations A' { short = '🖧1'; files = 'ops1.mis'; }
+defmap 'Operations B' { short = '🖧2'; files = 'ops2.mis'; }
+defmap 'Operations C' { short = '🖧3'; files = 'ops3.mis'; }
+defmap 'Operations D' { short = '🖧4'; files = 'ops4.mis'; }
 
-defmap 'Recreation 1' { short = '🏀1'; files = 'rec1.mis'; }
-defmap 'Recreation 2' { short = '🏀2'; files = 'rec2.mis'; }
-defmap 'Recreation 3' { short = '🏀3'; files = 'rec3.mis'; }
+defmap 'Recreation A' { short = '🏀1'; files = 'rec1.mis'; }
+defmap 'Recreation B' { short = '🏀2'; files = 'rec2.mis'; }
+defmap 'Recreation C' { short = '🏀3'; files = 'rec3.mis'; }
 
-defmap 'Command 1' { short = '✯1'; files = 'command1.mis'; }
-defmap 'Command 2' { short = '✯2'; files = 'command2.mis'; }
+defmap 'Command A' { short = '✯1'; files = 'command1.mis'; }
+defmap 'Command B' { short = '✯2'; files = 'command2.mis'; }
 
-defmap 'Rickenbacker 1' { short = '🗡1'; files = 'rick1.mis'; }
-defmap 'Rickenbacker 2' { short = '🗡2'; files = 'rick2.mis'; }
-defmap 'Rickenbacker 3' { short = '🗡3'; files = 'rick3.mis'; }
+defmap 'Rickenbacker Pod 1' { short = '🗡1'; files = 'rick1.mis'; }
+defmap 'Rickenbacker Pod 2' { short = '🗡2'; files = 'rick2.mis'; }
+defmap 'Rickenbacker Bridge' { short = '🗡3'; files = 'rick3.mis'; }
 
-defmap 'The Many' { short = '🧠'; files = 'many.mis'; }
-defmap 'SHODAN' { short = '🖳'; files = 'shodan.mis'; }
-
+defmap 'Body of the Many' { short = '🧠'; files = 'many.mis'; }
+defmap 'SHODAN C/space' { short = '🖳'; files = 'shodan.mis'; }
